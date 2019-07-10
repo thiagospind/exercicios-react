e8039cc4c5316256473e19ca357a9f009baf2d8b
Thiago Spindola <thiago_spdvr@hotmail.com>
2019-07-09 22:22:44 -0300

    aulas

--- src/componentes/Familia.jsx
+++ src/componentes/Familia.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { filhosComProps } from "../utils/utils";
 
 export default props =>
     <div>
         <h1>Família</h1>
-        {props.children}
+        {filhosComProps(props)}
+
+        {/* Forma mais utilizada para criar um componente, utilizando spread {...props}*/}
+        {/*{React.cloneElement(props.children, {...props})}*/}
+        {/*Forma menos usada para clonar o componente*/}
+        {/*{React.cloneElement(props.children, {props})}*/}
+        {/*{React.cloneElement(props.children,*/}
+        {/*    { sobrenome: props.sobrenome })}*/}
+        {/*{props.children}*/}
     </div>
--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ComponenteComFuncao from './componentes/ComponenteComFuncao'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
-import {CompA, CompB} from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
-import FamiliaSilva from './componentes/FamiliaSilva'
+// import {CompA, CompB} from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
+// import FamiliaSilva from './componentes/FamiliaSilva'
 import Familia from './componentes/Familia'
 import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <Familia sobrenome={"Pereira"}>
-            <Membro nome={"Andre"} />
-            <Membro nome={"Mariana"} />
-        </Familia>
+        <ComponenteComFuncao/>
+        {/*<Familia sobrenome={"Pereira"} numero={123}>*/}
+        {/*    <Membro nome={"Andre"} />*/}
+        {/*    /!*<Membro nome={"Mariana"} />*!/*/}
+        {/*</Familia>*/}
         {/*<MultiElementos/>*/}
         {/*<CompA valor={"Olá, eu sou A!"}/>*/}
         {/*<CompB valor={"B na área!"}/>*/}
         {/*/!*<PrimeiroComponente valor={"Bom dia!"} />*!/*/}
-        <FamiliaSilva/>
+        {/*<FamiliaSilva sobrenome={"Silva Pereira"}/>*/}
     </div>,
     elemento)
-
-
-
-
-
 
 // ReactDOM.render(<h1>Olá react 2</h1>, elemento)
