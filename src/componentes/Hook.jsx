bbb518edcef15046c4e8b3f0a3c399b52ba53025
Thiago Spindola <thiago.spindola@aapvr.com.br>
2019-08-13 17:22:46 -0300

    update

--- src/componentes/Hook.jsx
+++ src/componentes/Hook.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default props => {
     const [contador, setContador] = useState(100)
+    const [status, setParOuImpar] = useState(100)
 
+    useEffect(() => {
+        contador % 2 === 0 ? setParOuImpar('Par') : setParOuImpar('Impar')
+    })
     return (
         <div>
             <h1>{contador}</h1>
-            <button onClick={() => setContador(contador + 1)}>Inc</button>
+            <button
+                onClick={() => setContador(contador - 1)}>
+                Dec
+            </button>
+            <button
+                onClick={() => setContador(contador + 1)}>
+                Inc
+            </button>
         </div>
     )
 }
